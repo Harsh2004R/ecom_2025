7359587296b26f43d0419ec3ddcfe2637e52450f
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2025-08-05 12:59:18 +0530

    Added PageNotExist Ui and Updated Naviagtor for phone device

--- src/AppComponents/Navigator.jsx
+++ src/AppComponents/Navigator.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Container, Flex, Text, Button, Image, Box, Avatar, Center, Menu, Portal } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Container, Flex, Text, Button, Image, Box, Center, Menu, Portal } from '@chakra-ui/react'
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 import { Link } from 'react-router-dom';
 
 const Links = [
@@ -13,7 +14,7 @@ const Links = [
         path: "/auth",
     },
     {
-        name: "...",
+        name: "dummy url",
         path: "/1",
 
     }, {
@@ -34,25 +35,26 @@ const Links = [
 
 
 function Navigator() {
+    const [toggle, setToggle] = useState(false)
+    console.log(toggle);
     return (
         <Container
-            border={"1px solid red"}
+            // border={"1px solid red"}
+            borderBottom={"1px solid #DADADA"}
             maxW={"full"}
             minH={"60px"}
             px="2"
+            pos={"relative"}
+            zIndex={"1000"}
+
         >
-
-
-
-
 
 
             <Box display={{ base: "none", md: "block", lg: "block" }}>
 
-
                 <Flex>
                     <Box w={{ base: "40%", md: "40%", lg: "40%" }} h="60px" flex="1">
-                        <Center borderRadius={"full"} w="60px" h="60px" >
+                        <Center borderRadius={"full"} w="60px" h="60px" border={"1px solid lime"}>
                             <Menu.Root>
                                 <Menu.Trigger asChild>
                                     <Image borderRadius={"full"} w="90%" h="90%" src="https://avatar.iran.liara.run/public/1" alt="" />
@@ -62,9 +64,9 @@ function Navigator() {
                                     <Menu.Positioner>
                                         <Menu.Content>
                                             <Menu.ItemGroup>
-                                                <Menu.Item value="underline">Profile</Menu.Item>
-                                                <Menu.ItemGroupLabel>Login</Menu.ItemGroupLabel>
-                                                <Menu.Item value="bold">Logout</Menu.Item>
+                                                <Link to="/profile"> <Menu.ItemGroupLabel>Profile</Menu.ItemGroupLabel></Link>
+                                                <Link to="/auth"> <Menu.ItemGroupLabel>Login</Menu.ItemGroupLabel></Link>
+                                                <Link to="/"> <Menu.ItemGroupLabel >Logout</Menu.ItemGroupLabel></Link>
                                             </Menu.ItemGroup>
                                         </Menu.Content>
                                     </Menu.Positioner>
@@ -76,18 +78,60 @@ function Navigator() {
                         {
                             Links.map((el) => (
                                 <Center ml="20px" key={el.path}>
-                                    <Link to={el.path}><Text>{el.name}</Text></Link>
-
+                                    <Link to={el.path}><Text >{el.name}</Text></Link>
                                 </Center>
-
                             ))
                         }
                     </Flex>
                 </Flex>
             </Box>
 
+
+            {/*  Mobile Navigator from scratch... */}
+            <Box w="100%" h="60px" justifyContent={"space-between"} display={{ base: "flex", md: "none", lg: "none" }}>
+                <Center w="10%" h="100%">
+                    <GiHamburgerMenu onClick={() => setToggle((prev) => !prev)} size="25px" />
+
+                </Center>
+                {
+                    toggle && <Box bg="#18191aff" zIndex={"1001"} pos={"absolute"} top={"0"} left={"0"} borderRight={"1px solid #DADADA"} w="80%" minH={"100vh"}>
+                        <Center w="16%" flexDir={"column"} h="60px">
+                            <IoMdClose onClick={() => setToggle((prev) => !prev)} size="35px" />
+                        </Center>
+                        <Flex flexDir={"column"} w={"100%"} h="100%">
+                            {
+                                Links.map((el) => (
+                                    <Box mt="25px" padding={"2"} key={el.path} borderBottom={"0.5px solid #a5a5a5ff"}>
+
+                                        <Link to={el.path}><Text  >{el.name}</Text></Link>
+                                    </Box>
+                                ))
+                            }
+                        </Flex>
+                    </Box>
+                }
+
+                <Center borderRadius={"full"} w="60px" h="60px" border={"1px solid lime"}>
+                    <Menu.Root>
+                        <Menu.Trigger asChild>
+                            <Image borderRadius={"full"} w="90%" h="90%" src="https://avatar.iran.liara.run/public/1" alt="" />
+
+                        </Menu.Trigger>
+                        <Portal>
+                            <Menu.Positioner>
+                                <Menu.Content>
+                                    <Menu.ItemGroup>
+                                        <Link to="/profile"> <Menu.ItemGroupLabel>Profile</Menu.ItemGroupLabel></Link>
+                                        <Link to="/auth"> <Menu.ItemGroupLabel>Login</Menu.ItemGroupLabel></Link>
+                                        <Link to="/"> <Menu.ItemGroupLabel >Logout</Menu.ItemGroupLabel></Link>
+                                    </Menu.ItemGroup>
+                                </Menu.Content>
+                            </Menu.Positioner>
+                        </Portal>
+                    </Menu.Root>
+                </Center>
+            </Box>
         </Container>
     )
 }
-{/* <GiHamburgerMenu /> */ }
 export default Navigator
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -4,7 +4,6 @@ import Auth from '@/Pages/Auth'
 import Landing from '@/Pages/Landing'
 import PageNotExist from '@/Pages/PageNotExist'
 import React from 'react'
-import PasswordManager from '@/Pages/PasswordManager';
 function AllRoutes() {
     return (
         <>
@@ -13,7 +12,6 @@ function AllRoutes() {
                 <Route path='/' element={<Landing />} />
                 <Route path='*' element={<PageNotExist />} />
                 <Route path='/auth' element={<Auth />} />
-                <Route path='/manage-password' element={<PasswordManager />} />
                 {/* <Route path='/' element={<Landing />} /> */}
                 {/* <Route path='/' element={<Landing />} /> */}
                 {/* <Route path='/' element={<Landing />} /> */}
