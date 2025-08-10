5ee1ef6058c87d9c6c926176bc8eab24ae655c6e
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2025-08-10 10:31:50 +0530

    Added Chakra Ui components in Dashboard.jsx

--- src/Admin/Dashboard.jsx
+++ src/Admin/Dashboard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { Box ,Text} from "@chakra-ui/react";
 function Dashboard() {
   return (
-    <div>
-      Dashboard page.
-    </div>
+    <Box w="100%" minH={"100vh"}>
+      <Text textAlign={"center"}>Dashboard page.</Text>
+    </Box>
   )
 }
 
