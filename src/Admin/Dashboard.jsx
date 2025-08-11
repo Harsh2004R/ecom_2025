5a293fe109f155b1b877c90f218d1883c82a9cb9
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2025-08-11 11:29:16 +0530

    added dummy text for some reason

--- src/Admin/Dashboard.jsx
+++ src/Admin/Dashboard.jsx
@@ -3,7 +3,10 @@ import { Box ,Text} from "@chakra-ui/react";
 function Dashboard() {
   return (
     <Box w="100%" minH={"100vh"}>
-      <Text textAlign={"center"}>Dashboard page.</Text>
+      <Text textAlign={"center"}>Dashboard page</Text>
+      <Text>
+        dummy text added...
+      </Text>
     </Box>
   )
 }
